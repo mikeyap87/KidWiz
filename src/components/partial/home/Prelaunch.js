159227934063749cce2b75b67f047ebf693c99d1
nabeelelahi2k <nabeel.elahi@retrocube.org>
2023-07-20 09:31:26 +0500

    klaviyo done and test

--- src/components/partial/home/Prelaunch.js
+++ src/components/partial/home/Prelaunch.js
@@ -24,7 +24,7 @@ function PreLaunch() {
                 },
                 custom_source: 'Prelaunch Sign up!'
             },
-            relationships: { list: { data: { type: 'list', id: 'W8gFnX' } } }
+            relationships: { list: { data: { type: 'list', id: 'RzdCPx' } } }
         }
         const options = {
             method: 'POST',
