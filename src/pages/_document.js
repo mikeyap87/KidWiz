9d719515dc6360650d0a22420b0feac959abfb7e
mikeyap87 <127252762+mikeyap87@users.noreply.github.com>
2023-08-15 17:27:50 -0700

    Update _document.js
    Retune.so Chatbot

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+          {/* Updated chatbot script */}
+          <script
+            id="retune.so/chat"
+            src="https://retune.so/api/script/chat.js?id=11ee3bc9-97de-cb50-b060-27e1e8063720"
+            defer
+          ></script>
+        </Head>
       <body>
         <Main />
         <NextScript />
